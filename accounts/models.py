88c94a536f9a76e94f3e57d8816e8c1bcca4a996
Pavel-Nebolsin <ldrimanez@gmail.com>
2022-10-19 17:15:37 +0300

    default avatar added

--- accounts/models.py
+++ accounts/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE)
     birthdate = models.DateField(blank=True,null=True)
-    avatar = models.ImageField(upload_to="user_avatars/%Y/%m/%d",blank=True)
+    avatar = models.ImageField(upload_to="user_avatars/%Y/%m/%d",blank=True, default="user_avatars/default_avatar.JPG")
 
     def __str__(self):
         return f"Профиль пользователя {self.user.username}"
